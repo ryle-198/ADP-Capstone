85128682bcc3a810ca36a647c42897f8e62e9a58
Ryle <rylemay198@gmail.com>
2026-03-26 14:17:21 +0200

    add if-statement to check if values are null in DriverFactory

--- src/main/java/za/ac/cput/factory/DriverFactory.java
+++ src/main/java/za/ac/cput/factory/DriverFactory.java
@@ -2,12 +2,20 @@
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.Driver;
+import za.ac.cput.util.Helper;
+
 import java.util.UUID;
 
 public class DriverFactory {
     public static Driver buildDriver(String firstName, String lastName,
                                      String licenseNumber, String phoneNumber,
                                      boolean availability) {
+
+        if(Helper.isEmptyOrNull(firstName)|| Helper.isEmptyOrNull(lastName)
+        || Helper.isEmptyOrNull(licenseNumber)|| Helper.isEmptyOrNull(phoneNumber)){
+            return null;
+        }
+
         return new Driver.Builder()
                 .setId(UUID.randomUUID().toString())
                 .setFirstName(firstName)
