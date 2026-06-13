d92f0892f5a95cf2f7037e30ba96fa4613fbb570
Ryle <rylemay198@gmail.com>
2026-06-13 15:36:39 +0200

    Completed DO controller

--- src/main/java/za/ac/cput/controller/DeliveryOrdersController.java
+++ src/main/java/za/ac/cput/controller/DeliveryOrdersController.java
@@ -1,4 +1,43 @@
 package za.ac.cput.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.DeliveryOrders;
+import za.ac.cput.service.DeliveryOrdersService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")//to-do
 public class DeliveryOrdersController {
+    private DeliveryOrdersService service;
+
+    @Autowired
+    DeliveryOrdersController(DeliveryOrdersService service){this.service=service;}
+
+    @PostMapping("create")
+    public DeliveryOrders createDeliveryOrder(@RequestBody DeliveryOrders deliveryOrders){
+        return this.service.create(deliveryOrders);
+    }
+
+    @GetMapping("/read/{deliveryOrderId}")
+    public DeliveryOrders readDeliveryOrder(@PathVariable String deliveryOrderId){
+        return this.service.read(deliveryOrderId);
+    }
+
+    @PostMapping("/update")
+    public DeliveryOrders updateDeliveryOrder(@RequestBody DeliveryOrders updatedDeliveryOrders){
+        return this.service.update(updatedDeliveryOrders);
+    }
+
+    @DeleteMapping("/delete/{deliveryOrderId}")
+    public boolean deleteOrder(String deliveryOrderId){
+        return this.service.delete(deliveryOrderId);
+    }
+
+    @GetMapping("/getAllDeliveryOrders")
+    public List<DeliveryOrders> getAllDeliveryOrders(){
+        return this.service.getAllDeliveryOrders();
+    }
+
 }
